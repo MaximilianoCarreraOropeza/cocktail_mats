95b9fd60ef90b0a212a0c92b52ee70317e67ca1f
unknown <20223tn012@utez.edu.mx>
2025-10-21 17:40:24 -0600

    9 commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', ()=>{
-        navigator.serviceWorker.register('/cocktail_mats/sw.js')
+        navigator.serviceWorker.register('./cocktail_mats/sw.js')
         .then(req => console.log('Registro SW exitoso', req))
         .catch(err => console.log('Error de registro SW:', err))
     })
